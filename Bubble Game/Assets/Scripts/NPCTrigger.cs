c7fbaa6d9a7a5833053c62e0ada2560c9345039a
bwolfin1 <bwolfin1@jhu.edu>
2025-01-25 15:30:02 -0500

    Added npc spawner and put things into folders

--- Bubble Game/Assets/Scripts/NPCTrigger.cs
+++ Bubble Game/Assets/Scripts/NPCTrigger.cs
@@ -6,14 +6,14 @@ using UnityEngine;
 public class NPCTrigger : MonoBehaviour
 {
     public static Action NPCTriggered = delegate { };
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        print("skibidi rizzler");
-    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        print("skibidi rizzler");
-        NPCTriggered.Invoke();
+        if(collision.gameObject.tag == "Player")
+        {
+            Destroy(this.gameObject);
+            NPCTriggered.Invoke();
+        }
+
     }
 }
