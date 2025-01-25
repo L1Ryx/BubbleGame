8f596233216b4d3f4ce70bd47922c4becc24665d
bwolfin1 <bwolfin1@jhu.edu>
2025-01-25 14:20:46 -0500

    added some of the starting scripts such as player movement, and the spotlight changes as well as npc trigger.

---
+++ Bubble Game/Assets/Scripts/LightController.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.Universal;
+
+public class LightController : MonoBehaviour
+{
+    [SerializeField]
+    private float startingRadius = 5.0f;
+
+    [SerializeField]
+    private float radiusIncrease = 1.0f;
+
+    [SerializeField]
+    private float radiusDecay = .05f;
+
+    Light2D spotLight;
+    CircleCollider2D lightCollider;
+
+    private void OnEnable()
+    {
+        NPCTrigger.NPCTriggered += IncreaseSize;
+    }
+
+    private void OnDisable()
+    {
+        NPCTrigger.NPCTriggered -= IncreaseSize;
+    }
+
+    private void Awake()
+    {
+        spotLight = GetComponentInParent<Light2D>();
+        lightCollider = GetComponentInParent<CircleCollider2D>();
+    }
+
+    private void Start()
+    {
+        spotLight.pointLightOuterRadius = startingRadius;
+        lightCollider.radius = startingRadius;
+    }
+
+    private void Update()
+    {
+        spotLight.pointLightOuterRadius -= radiusDecay;
+        lightCollider.radius -= radiusDecay;
+        if (spotLight.pointLightOuterRadius <= 0)
+        {
+            print("You lose and you suck");
+        }
+    }
+
+    private void IncreaseSize()
+    {
+        spotLight.pointLightOuterRadius += radiusIncrease;
+        lightCollider.radius += radiusIncrease;
+    }
+}
